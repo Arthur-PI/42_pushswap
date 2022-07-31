8920ee7cf758fb64d09d6e051816a85e4b7aa1b0
Arthur Pigeon <pigeon.arthur@hotmail.fr>
2022-08-01 01:17:35 +0200

    checker done

--- src/operations.c
+++ src/operations.c
@@ -6,7 +6,7 @@
 /*   By: arthur <arthur@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/12 16:31:50 by apigeon           #+#    #+#             */
-/*   Updated: 2022/07/30 22:57:37 by apigeon          ###   ########.fr       */
+/*   Updated: 2022/08/01 00:57:46 by apigeon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,7 @@ void	push(t_stack **a, t_stack **b)
 {
 	t_stack	*tmp;
 
-	if (!b || !a)
+	if (*b == NULL)
 		return ;
 	tmp = *b;
 	*b = tmp->next;
@@ -62,6 +62,8 @@ void	rotate_reverse(t_stack **stack)
 	t_stack	*prev;
 	t_stack	*curr;
 
+	if (*stack == NULL)
+		return ;
 	first = *stack;
 	prev = first;
 	curr = prev;
--- test.sh
+++ test.sh
@@ -2,7 +2,6 @@
 
 ARG="$@"
 # Verify if the pushswap program does in fact sort the list
-./push_swap $ARG | ./checker_mac $ARG
 ./push_swap $ARG | ./checker $ARG
 # Gives the number of operations that pushswap made
 ./push_swap $ARG | wc -l
